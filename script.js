b2493461e1b1f0ee2a8ebc45217cdda526498c3f
Sammy Erickson <0uts1d3th3b0x355@gmail.com>
2022-07-07 08:25:51 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,31 @@
+let name1 = prompt("Please give me a name.");
+let name2 = prompt("Please give me another name.");
+let name3 =prompt("Please give me a third name.");
+let num1 = name1.length;
+let num2 = name2.length;
+let num3 = name3.length;
+
+console.log(name1);
+console.log(name2);
+console.log(name3);
+console.log(num1);
+console.log(num2);
+console.log(num3);
+
+function longestName(number1, number2, number3){
+    if (number1 >number2 && number1> number3){
+        console.log(`${name1} has the longest name`);
+    } else if(number2 > number1 && number2 > number3){
+        console.log(`${name2} has the longest name`); 
+    } else if(number3 > number1 && number3 >number2){
+        console.log(`${name3} has the longest name`);
+    }else if(number1==number2 && number1>number3){
+        console.log(`${name1} and ${name2} tie for the longest name`);
+    }else if(number2 == number3 && number2>number1){
+        console.log(`${name2} and ${name3} tie for the longest name`);
+    }else{
+        console.log(`All three names, ${name1}, ${name2}, and ${name3}, are all the same length`);
+    }
+}
+
+ longestName(num1, num2, num3);
